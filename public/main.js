74b042a85a56c2bfc156d496eb4dc49c1e109bee
Kevin <lopezkevin175@gmail.com>
2018-05-02 23:53:11 -0400

    added update method

---
+++ public/main.js
@@ -0,0 +1,18 @@
+const update = document.getElementById('update')
+
+update.addEventListener('click', () => {
+  fetch('quotes', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'name': 'Darth Vader',
+      'quote': 'I find your lack of faith disturbing.'
+    })
+  })
+  .then(response => {
+    if (response.ok) return response.json()
+  })
+  .then(data => {
+    console.log(data)
+  })
+});
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ let db;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(bodyParser.json())
+
 
 
 MongoClient.connect('mongodb://kevin:starwars@ds161459.mlab.com:61459/star-wars-quotes', (err, client) => {
@@ -37,3 +40,19 @@ app.post('/quotes', (req, res) => {
     }
   });
 });
+
+app.put('/quotes', (req, res) => {
+  db.collection('quotes')
+  .findOneAndUpdate({name: 'Yoda'}, {
+    $set: {
+      name: req.body.name,
+      quote: req.body.quote
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
